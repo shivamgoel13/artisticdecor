2949f4b8101fddca386262298cedbfd0b94e8b5d
ShivamGoyal <shivam.goyal@squareyards.co.in>
2023-07-01 11:32:47 +0530

    bg video

--- public/assets/js/walls.js
+++ public/assets/js/walls.js
@@ -30,14 +30,3 @@ $(document).ready(function() {
         } 
     });  
   });
-//   $(document).ready(function() {
-//     var autoplaySlider = $('#autoplay').lightSlider({
-//         auto:true,
-//         loop:true,
-//         pauseOnHover: true,
-//         onBeforeSlide: function (el) {
-//             $('#current').text(el.getCurrentSlideCount());
-//         } 
-//     });
-//     $('#total').text(autoplaySlider.getTotalSlideCount());
-// });
